3b5fe750345712b5196ca425174b268e112e76cd
Nandhini Shri <nandhinishri7002@gmail.com>
2026-08-30 17:39:55 +0530

    Add category filtering

--- store/views.py
+++ store/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Book
+from .models import Book, Category
 
 
 def book_list(request):
     books = Book.objects.all()
+    categories = Category.objects.all()
 
     search_query = request.GET.get('search')
+    category_id = request.GET.get('category')
 
     if search_query:
         books = books.filter(
@@ -14,9 +16,14 @@ def book_list(request):
             author__icontains=search_query
         )
 
+    if category_id:
+        books = books.filter(category_id=category_id)
+
     return render(request, 'books/book_list.html', {
         'books': books,
+        'categories': categories,
         'search_query': search_query,
+        'selected_category': category_id,
     })
 
 
