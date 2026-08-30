8e6ac57325b8f4d29e67684343eb8e37ea52c1e1
Nandhini Shri <nandhinishri7002@gmail.com>
2026-08-30 16:54:54 +0530

    Complete books and search module

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category, Book, Subscriber, Cart, Order, OrderItem
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'author',
+        'category',
+        'price',
+        'is_best_seller',
+        'is_new_arrival',
+    )
+    search_fields = ('title', 'author')
+
+
+admin.site.register(Subscriber)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
-# Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+
+    search_query = request.GET.get('search')
+
+    if search_query:
+        books = books.filter(
+            title__icontains=search_query
+        ) | books.filter(
+            author__icontains=search_query
+        )
+
+    return render(request, 'books/book_list.html', {
+        'books': books,
+        'search_query': search_query,
+    })
+
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    return render(request, 'books/book_detail.html', {
+        'book': book,
+    })
